54840d7f899b54dbc65d89900c21626aaf4cee66
gabocv0205-ship-it <gabocv0205@gmail.com>
2026-05-21 10:53:38 -0500

    Create server.ts

---
+++ lib/server.ts
@@ -0,0 +1,33 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export function createClient() {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        async get(name: string) {
+          const cookieStore = await cookies()
+          return cookieStore.get(name)?.value
+        },
+        async set(name: string, value: string, options: CookieOptions) {
+          try {
+            const cookieStore = await cookies()
+            cookieStore.set({ name, value, ...options })
+          } catch (error) {
+            // Manejado desde el middleware
+          }
+        },
+        async remove(name: string, options: CookieOptions) {
+          try {
+            const cookieStore = await cookies()
+            cookieStore.set({ name, value: '', ...options })
+          } catch (error) {
+            // Manejado desde el middleware
+          }
+        },
+      },
+    }
+  )
+}
